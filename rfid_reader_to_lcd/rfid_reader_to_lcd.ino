bb2205b81068eca44168baa2e5c83f2839d88780
John Ryan <john@lifeforms.ie>
2012-02-04 15:46:16 -0800

    Arduino now just reads RFID & transmits it via serial

--- rfid_reader_to_lcd/rfid_reader_to_lcd.ino
+++ rfid_reader_to_lcd/rfid_reader_to_lcd.ino
@@ -8,13 +8,15 @@ int speakerPin = 6;
 
 void setup() { 
   Serial.begin(2400);                    // RFID reader SOUT pin connected to Serial RX pin at 2400bps 
-  pinMode(7,OUTPUT);                     // Set digital pin 2 as OUTPUT to connect it to the RFID /ENABLE pin 
+  pinMode(7,OUTPUT);                     // Set digital pin 2 as OUTPUT to connect it to the RFID /ENABLE pin
   digitalWrite(7, LOW);                  // Activate the RFID reader
-  pinMode(speakerPin, OUTPUT);                    // Set pin for speaker output
+  pinMode(speakerPin, OUTPUT);           // Set pin for speaker output
   
   lcd.begin(16, 2);                      // set up the LCD's number of columns and rows
   lcd.setCursor(0, 0);
-  lcd.print("I am waiting... ");
+  lcd.print("->P.A.U.S.E.S.<-");
+  lcd.setCursor(0, 1);
+  lcd.print("Initializing...");
 }  
 
 void loop() {
@@ -32,26 +34,8 @@ void loop() {
         } 
       } 
       if(bytesread == 10) {              // if 10 digit read is complete 
-        Serial.print("TAG code is:   "); // possibly a good TAG 
+        Serial.print("RFID="); // possibly a good TAG 
         Serial.println(code);            // print the TAG code
-        lcd.setCursor(0, 0);
-        lcd.print("I am currently ");
-        lcd.setCursor(0, 1);
-        if(code[8] == 'B') {
-          lcd.print("clapping        ");
-        }
-        if(code[8] == '7') {
-          lcd.print("CARD #2         ");
-        }
-        if(code[8] == '9') {
-          lcd.print("making a call.  ");
-        }
-        if(code[8] == '1') {
-          lcd.print("sharpening.     ");
-        }
-        if(code[8] == '8') {
-          lcd.print("holding a cup.  ");
-        }
       } 
       bytesread = 0; 
       digitalWrite(7, HIGH);             // deactivate the RFID reader for a moment so it will not flood
@@ -60,10 +44,6 @@ void loop() {
       digitalWrite(speakerPin, LOW);     // turn the speaker off
       delay(1000);                       // wait for a bit
       digitalWrite(7, LOW);              // Activate the RFID reader
-      lcd.setCursor(0, 0);
-      lcd.print("I am waiting... ");
-      lcd.setCursor(0, 1);
-      lcd.print("                ");
     } 
   }
 } 
